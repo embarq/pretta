b7fb9eb3bccf475d52ba65b60024674029397819
embarq <embarq.dev@gmail.com>
2016-12-15 19:02:58 +0200

    Implement dummy player

--- www/js/app.js
+++ www/js/app.js
@@ -7,15 +7,133 @@
     })
     .run(function($ionicPlatform) {
       $ionicPlatform.ready(function() {
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          cordova.plugins.Keyboard.disableScroll(true);
+        return window.StatusBar && StatusBar.styleDefault();
+      });
+    })
+    .controller('PlaybackController', function($ionicPlatform) {
+      var vm = this;
+      
+      /**
+      * @description helper function for next/prev functionality
+      * @return {number} current track index
+      */
+      var getCurrentTrackIndex = function(track) {
+        return vm.tracks.findIndex(function(_track) {
+          return track.id === _track.id;
+        }) || null;
+      };
+
+      /**
+      * @description here you can track player status
+      * @link http://cordova.apache.org/docs/en/latest/reference/cordova-plugin-media/index.html#constants
+      */
+      var handleStatusChange = function(status) {
+        if (status === Media.MEDIA_RUNNING) {
+          console.log('Now playing', vm.track.title);
+        }
+      };
+
+      /**
+      * @description here you can handle controls in notifications or lock-screen
+      */
+      var handleMusicControls = function(action) {
+        switch(action) {
+          case 'music-controls-next':
+            return vm.next();
+          case 'music-controls-previous':
+            return vm.prev();
+          case 'music-controls-pause':
+            return vm.pause();
+          case 'music-controls-play':
+            return vm.play();
+          case 'music-controls-destroy':
+            return vm.reset();
+
+          // Headset events (Android only)
+          case 'music-controls-media-button' :
+            return null;
+          case 'music-controls-headset-unplugged':
+            return null;
+          case 'music-controls-headset-plugged':
+            return null;
+
+          default:
+            return null;
+        }
+      }
+
+      var getMediaInstance = function(src) {
+        return new Media(src, null, null, handleStatusChange);
+      };
+
+      /**
+      * @description creates controls in notifications pane and lock-screen
+      * @link https://github.com/homerours/cordova-music-controls-plugin#methods
+      */
+      var createMusicControls = function(track) {
+        return MusicControls.create({
+          track: track.title,
+          artist: track.user,
+          ticker: `Now playing ${ track.title }`,
+          dismissable : true
+        });
+      };
+
+      vm.tracks = [];
+      vm.track = null;
+      vm.currentTrackMeta = null;
+
+      vm.addTrack = function(track) {
+        return vm.tracks.push({
+          id: track.id,
+          user: track.user.username,
+          title: track.title,
+          src: track.src,
+          duration: track.duration
+        });
+      };
+
+      vm.play = function(index) {
+        if (index != null) {
+          MusicControls.destroy();
+
+          createMusicControls(vm.tracks[index]);
+
+          vm.track != null && vm.track.stop();
+
+          vm.track = getMediaInstance(vm.tracks[index].src);
+          vm.currentTrackMeta = vm.tracks[index]; 
+        } else {
+          MusicControls.updateIsPlaying(true);
         }
 
-        if(window.StatusBar) {
-          StatusBar.styleDefault();
-        }
+        return vm.track.play();
+      };
+
+      vm.pause = function() {
+        MusicControls.updateIsPlaying(false);
+        vm.track != null && vm.track.pause();
+      };
+
+      vm.next = function() {
+        var currentTrackIndex = getCurrentTrackIndex(vm.currentTrackMeta);
+        return vm.play((currentTrackIndex + 1) >= vm.tracks.length ? 0 : currentTrackIndex + 1);
+      };
+
+      vm.prev = function() {
+        var currentTrackIndex = getCurrentTrackIndex(vm.currentTrackMeta);
+        return vm.play((currentTrackIndex - 1) <= 0 ? vm.tracks.length - 1 : currentTrackIndex - 1);
+      };
+
+      vm.reset = function() {
+        vm.track.stop();
+        vm.track = null;
+      }
+
+      $ionicPlatform.ready(function() {
+        MusicControls.subscribe(handleMusicControls);
+        MusicControls.listen();
       });
-    });
+    })
 
 })();
